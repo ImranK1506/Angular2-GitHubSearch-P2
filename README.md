91c727c0716a4ee518c86983033f7c8a4ed6fa62
Imran Khan <m_imran_khan@me.com>
2017-07-05 01:23:16 -0400

    user profiles and additional data added

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<h1>Angular 2 - GitHubSearch</h1>
+<h1>Angular 4.2.5 - GitHubSearch</h1>
 
 <h2>Author</h2>
 Imran Khan
--- src/app/components/profile.component.ts
+++ src/app/components/profile.component.ts
@@ -6,10 +6,13 @@ import {GithubService} from '../services/github.service';
   selector: 'profile',
   templateUrl: 'profile.component.html',
 })
+
 export class ProfileComponent  {
+  user:any[];
+
   constructor(private _githubService: GithubService){
-    this._githubService.getUser().subscribe(user =>{
-      console.log(user);
+    this._githubService.getUser().subscribe(user => {
+      this.user = user;
     });
   } 
  }
